fdb18136bf5ba1bbe22dfac053bd3046366bd3a0
Gabriel447 <fullstackgabe@gmail.com>
2023-09-12 19:14:54 -0300

    código final

--- src/Contador.java
+++ src/Contador.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Contador {
+
+import java.util.Scanner;
+public class Contador {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro:");
+        int n1 = sc.nextInt();
+        System.out.println("Digite o segundo parâmetro:");
+        int n2 = sc.nextInt();
+
+        try {
+            contar(n1, n2);
+        }catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+    static void contar(int n1, int n2 ) throws ParametrosInvalidosException{
+        if(n1 < n2){
+            for (int i = n1;i <= n2; i++){
+                System.out.print(i + " ");
+            }
+        }else {
+            throw new ParametrosInvalidosException();
+        }
+    }
 }
